9e6992e809f333af68e0112f1c4d367de400ba3b
zramsay <zach@bluecollarcoding.ca>
2024-01-22 11:15:55 -0500

    should use env var but w/e

---
+++ scripts/create-metadata-record.sh
@@ -0,0 +1,17 @@
+
+
+record:
+  type: GeneralRecord
+  name: my-generic-record
+  version: 0.0.1
+  value: "anything-goes-here"
+  category: filter-by-this
+  tags:
+    - a
+    - b
+    - c
+  meta:
+    foo: bar
+    bar:
+      baz: boz
+
