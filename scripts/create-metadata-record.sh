cdf73acae44c6950dda340d8ee84190759ee7924
zramsay <zach@bluecollarcoding.ca>
2024-02-02 17:09:18 -0500

    use testnet

--- scripts/create-metadata-record.sh
+++ scripts/create-metadata-record.sh
@@ -73,8 +73,8 @@ EOF
 cat <<EOF > "$CONFIG_FILE"
 services:
   cns:
-    restEndpoint: '${CERC_REGISTRY_REST_ENDPOINT:-http://138.197.130.188:1317}'
-    gqlEndpoint: '${CERC_REGISTRY_GQL_ENDPOINT:-http://138.197.130.188:9473/api}'
+    restEndpoint: '${CERC_REGISTRY_REST_ENDPOINT:-http://console.laconic.com:1317}'
+    gqlEndpoint: '${CERC_REGISTRY_GQL_ENDPOINT:-http://console.laconic.com:9473/api}'
     chainId: ${CERC_REGISTRY_CHAIN_ID:-laconic_9000-1}
     gas: 550000
     fees: 200000aphoton
